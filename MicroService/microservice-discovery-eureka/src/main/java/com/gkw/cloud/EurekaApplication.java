881454848e061492f26343c10efed2e0f8848ef9
高孔威 <879524485@qq.com>
2019-10-24 10:55:50 +0800

    spring-cloud基础配置

---
+++ MicroService/microservice-discovery-eureka/src/main/java/com/gkw/cloud/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.gkw.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//声明:服务发现
+public class EurekaApplication {
+
+
+
+        public static void main(String[] args) {
+
+            SpringApplication.run(EurekaApplication.class,args);
+        }
+
+}
