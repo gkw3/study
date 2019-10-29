b5b21bb377ca5335d276b05d6adb97e15e5f26ac
高孔威 <879524485@qq.com>
2019-10-29 11:32:46 +0800

    eureka配置

--- MicroService/microservice-provider-user/src/main/java/com/gkw/cloud/controller/UserController.java
+++ MicroService/microservice-provider-user/src/main/java/com/gkw/cloud/controller/UserController.java
@@ -2,12 +2,17 @@ package com.gkw.cloud.controller;
 
 import com.gkw.cloud.entity.User;
 import com.gkw.cloud.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -16,6 +21,15 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private Registration registration; // 服务注册
 
     @GetMapping("/simple/{id}")
     public User findById(@PathVariable  Long id) {
@@ -29,6 +43,34 @@ public class UserController {
             return  null;
         }
 
+    }
+
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl(){
+        InstanceInfo instance= eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER",false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo(){
+
+        //2.0以后的方法
+        ServiceInstance instance = serviceInstance();
+        return instance;
+
+    }
+
+
+    public ServiceInstance serviceInstance() {
+        List<ServiceInstance> list = discoveryClient.getInstances(registration.getServiceId());
+        if (list != null && list.size() > 0) {
+            for(ServiceInstance itm : list){
+                if(itm.getPort() == 7900)
+                    return itm;
+            }
+        }
+        return null;
     }
 
 
