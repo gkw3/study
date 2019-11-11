7d2e444bdfb8fbbc142b316eb3de2869a99af4c8
高孔威 <879524485@qq.com>
2019-11-11 14:32:16 +0800

    ribbon基本配置使用

--- MicroService/microservice-consumer-movie-ribbon/src/main/java/com/gkw/cloud/MicroserviceSimpleConsumerMovieRibbonApplication.java
+++ MicroService/microservice-consumer-movie-ribbon/src/main/java/com/gkw/cloud/MicroserviceSimpleConsumerMovieRibbonApplication.java
@@ -2,22 +2,22 @@ package com.gkw.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MicroserviceSimpleConsumerMovieApplication {
+public class MicroserviceSimpleConsumerMovieRibbonApplication {
 
 	@Bean
-	public RestTemplate restTemplate(){
+	@LoadBalanced
+	public  RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
-
-
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+		SpringApplication.run(MicroserviceSimpleConsumerMovieRibbonApplication.class, args);
 	}
 
 }
